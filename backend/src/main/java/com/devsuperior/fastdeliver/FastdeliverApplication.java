2fb2da1309c5ee1867fe40e67a9bbc49ac95c741
ArtNascto <arthurgnascto@gmail.com>
2021-01-09 16:39:08 -0300

    Initial commit

---
+++ backend/src/main/java/com/devsuperior/fastdeliver/FastdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.fastdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/fastdeliver/FastdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.fastdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
